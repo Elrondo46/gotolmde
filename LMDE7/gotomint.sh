46c6a0b3a5c54e16fbabf03e98bbf625a91aa72f
Elrondo46 <elrond94@users.noreply.github.com>
2025-09-20 18:35:49 +0200

    First version for testing

---
+++ LMDE7/gotomint.sh
@@ -0,0 +1,21 @@
+echo "Welcome to Debian to LMDE tool. This tool will convert your debian to LMDE"
+sleep 5
+
+#Installing GPG tool
+apt install -y gpg
+
+#Removing old sources
+rm /etc/apt/sources.list
+rm /etc/apt/trusted.gpg.d
+#Installing new keys and sources
+cp -r trusted.gpg.d /etc/apt/trusted.gpg.d
+cp official-package-repositories.list /etc/apt/sources.list.d/official-package-repositories.list
+echo "Converting Now..."
+sleep 3
+apt update && apt upgrade -y
+xargs apt-get install -y < list-packages-mint
+echo "If no errors... That's complete
+sleep 2
+echo "Post-Concerting config"
+rm /etc/network/interfaces
+echo "Finished. Can reboot now"
